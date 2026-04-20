de59d73d09110ec7d0fc2c0a16bb8311bb7030bb
Divyanshi Shukla <divyanshishukla331@gmail.com>
2026-04-20 22:46:00 +0530

    Create fun_chatbox_using_ifelse.py

---
+++ fun_chatbox_using_ifelse.py
@@ -0,0 +1,12 @@
+msg = input("You: ")
+
+if msg == "hello":
+    print("Bot: Hello human!")
+elif msg == "how are you":
+    print("Bot: I am fine, what about you?")
+elif msg == "who made you":
+    print("Bot: I was created by Python code.")
+elif msg == "what is ai":
+    print("AI means artificial intelligence ")
+else:
+    print("Bot: Interesting...")
